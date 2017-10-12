6e770f0fe3294dec7276eff86c04ec72f16000ff
acorzo <alexecorzo@gmail.com>
2017-10-12 16:52:56 +0000

    Quality scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170929212336) do
+ActiveRecord::Schema.define(version: 20170929220931) do
 
   create_table "passions", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "qualities", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "kind"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_qualities_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
---
+++ test/system/qualities_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class QualitiesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit qualities_url
+  #
+  #   assert_selector "h1", text: "Quality"
+  # end
+end
